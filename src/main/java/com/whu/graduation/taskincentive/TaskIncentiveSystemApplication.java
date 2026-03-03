be95f9d60255bf7fed7688cc0bcb1da4b67aa344
董华强 <1786480231@qq.com>
2026-03-03 14:42:01 +0800

    项目初始化

---
+++ src/main/java/com/whu/graduation/taskincentive/TaskIncentiveSystemApplication.java
@@ -0,0 +1,13 @@
+package com.whu.graduation.taskincentive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskIncentiveSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskIncentiveSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whu/graduation/taskincentive/TaskIncentiveSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whu.graduation.taskincentive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskIncentiveSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
